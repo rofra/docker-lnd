a2ae32ced1477cea17b32b2f8267243c5742876d
Rodolphe FRANCESCHI <fedorage@gmail.com>
2021-07-31 11:38:18 +0200

    Source removed

--- run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-# Launch lnd daemon
-lnd --nolisten --bitcoin.active --bitcoin.mainnet --debuglevel=debug --bitcoin.node=bitcoind  --bitcoind.rpcuser=api --bitcoind.rpcpass=api --bitcoind.zmqpubrawblock=tcp://127.0.0.1:28332 --bitcoind.zmqpubrawtx=tcp://127.0.0.1:28333 &
-sleep 10
-
-# Test client and daemon
-lncli --version
-lnd -V
